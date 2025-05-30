a629661648be389823910f8dee1a1d8076741753
pvalvaro <pvalvaro25@gmail.com>
2025-05-30 18:19:29 -0300

    ajustes nos metodos getAll do usercontroller e coursecontroller

--- authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -1,6 +1,10 @@
 package com.ead.authuser.specifications;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
 import net.kaczmarzyk.spring.data.jpa.domain.Empty;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
@@ -8,6 +12,9 @@ import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Collection;
+import java.util.UUID;
 
 public class SpecificationTemplate {
     @And({
@@ -18,4 +25,11 @@ public class SpecificationTemplate {
             @Spec(path = "fullName", spec = LikeIgnoreCase.class)
     })
     public interface UserSpec extends Specification<UserModel>{}
+    public static Specification<UserModel> userCourseId(final UUID courseId){
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Join<UserModel, UserCourseModel> userJoin = root.join("userCourses");
+            return cb.equal(userJoin.get("courseId"), courseId);
+        };
+    }
 }
